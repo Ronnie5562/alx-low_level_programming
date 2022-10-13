05e5ea4e6f3bc0cd155346771f7a18bdf2748ba4
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-10-13 16:14:54 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,8 @@
- #include <stdio.h>
-
-/*
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     
- */
+#include <stdio.h>
 
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
 
 }
