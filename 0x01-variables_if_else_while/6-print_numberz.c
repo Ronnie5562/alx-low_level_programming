77f083448bc474c2696271f70f97b83b76852765
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-10-16 14:42:21 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - prints numbers in base 10
  * Description: all base 10 single digit
- * Return: 0
+ * Return: 0 (success)
  */
 int main(void)
 {
-	int x = 0;
+	int x;
 
-	while (x < 10)
+	for (x = 0; x < 10; x++)
 	{
-		putchar(x);
-		x++;
+		putchar(x + '0');
 	}
-
 	putchar('\n');
 	return (0);
 }
