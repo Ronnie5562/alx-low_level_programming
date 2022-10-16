07d28dd24f82ab2dfb95eafba630c4d09b458de6
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-10-16 14:13:41 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints out numbers in base 10
+ * Description: single digits in base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
