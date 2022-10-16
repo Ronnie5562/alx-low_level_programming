2b1f75477342c3d5405074167d08df9390da9017
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-10-16 16:11:23 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints size of each data type
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a char: %zu byte(s)\n", sizeof(int));
+	printf("Size of a char: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a char: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a char: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
