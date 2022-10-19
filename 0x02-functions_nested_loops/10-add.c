3490bd68163ce989009ecb451fff4a6fd7e450d1
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-10-18 18:33:18 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @x: integer 1
+ * @y: integer 2
+ * Description: give two integers, returns sum
+ * Return: sum
+ */
+
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,7 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif
