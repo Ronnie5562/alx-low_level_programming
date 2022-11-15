d9fb0af330243c51f5995e272a47ed135bb80567
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-11-15 10:56:12 -0800

    Task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,8 +3,7 @@
 
 /**
  * File: 0-object_like_macro.h
- * Desc: Header file that defines a macro named
- * 	SIZE as an abbreviation for the token 1024.
+ * Desc: Header file that defines a macro named SIZE for the token 1024.
  */
 
 #define SIZE 1024
