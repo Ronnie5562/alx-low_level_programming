547edb60e88874d60bf74220c767811e79f4c747
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-11-15 15:24:38 -0800

    Task 4

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,10 +1,6 @@
 #ifndef SUM_H
 #define SUM_H
-/**
- * File: 4-sum.h
- * Desc: Header file that defines a function-like macro SUM(x, y)
- * that computes the sum of the numbers x and y.
- */
+
 #define SUM(x, y) ((x) + (y))
 
-#endif /*SUM_H*/
+#endif
