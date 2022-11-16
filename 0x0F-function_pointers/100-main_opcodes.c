a18d88933d73165830ca969179928f6a6a4020da
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-11-16 14:58:38 -0800

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -32,8 +32,10 @@ int main(int argc, char *argv[])
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
+		{
 			printf("%02hhx\n", arr[i]);
 			break;
+		}
 		printf("%02hhx ", arr[i]);
 	}
 	return (0);
