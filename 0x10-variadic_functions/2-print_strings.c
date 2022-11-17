f6bec6332ef0ef064ee858da8b5da1816e20d658
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-11-17 13:21:02 -0800

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(str, char*);
+		str = va_arg(str, char *);
 
 		if (str == NULL)
 			printf("(nil)");
