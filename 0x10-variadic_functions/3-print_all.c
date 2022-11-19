492bf7319a0c49506eee6aa28cc02d5d3c9d2526
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-11-18 23:56:43 -0800

    Final Task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -84,17 +84,17 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 
-	while (format && (*(format + i)))
+	while (format && format[i])
 	{
 		j = 0;
 
-		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+		while (j < 4 && (format[i] != *(funcs[j].symbol)))
 			j++;
 
 		if (j < 4)
 		{
 			printf("%s", separator);
-			funcs[j].print(args);
+			funcs[j].print_func(args);
 			separator = ", ";
 		}
 
