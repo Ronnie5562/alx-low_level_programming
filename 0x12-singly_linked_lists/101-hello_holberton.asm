021beec057a6512eb3c2003e90d5b1e91fe42bb5
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2022-11-25 00:49:27 +0100

    Update 101-hello_holberton.asm
     Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,3 @@
-; My comments: It is so fun to write in assembly language
-; File: 101-hello_holberton.asm
-; Auth: Firdaus H. Salim
-; Desc: 64-bit assembly program that prints
-;       Hello, Holberton followed by a new line.
-
 extern printf
 
 section .text
