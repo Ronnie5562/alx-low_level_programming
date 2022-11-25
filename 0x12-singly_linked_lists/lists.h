859b883a81c95d14b4d3d735fbd0694fe71bad5f
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2022-11-25 01:04:28 +0100

    Update lists.h
    I fixed a bug.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/**
- * Desc: header file containinf prototypes and definitions
- * for all functions and types written in this proj
- */
-
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -27,4 +24,4 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t *head, const char *str);
-
+#endif
